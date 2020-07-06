303e238d3e4eaddf157888437cdbe1e3905c2f88
Dileep <dileep@onetikk.com>
2020-07-06 08:58:48 +0530

    Initial commit

---
+++ registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Onetikk_M2_LightGallery',
+    __DIR__
+);
---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,12 @@
+
+var config = {
+    "map": {
+        "*": {
+            "LightGallery": "Onetikk_M2_LightGallery/js/lightgallery"
+        }
+    },
+    "shim":{
+        "Onetikk_M2_LightGallery/js/lightgallery.min": ["jquery"],
+        "Onetikk_M2_LightGallery/js/lg-zoom.min": ["jquery"]
+    }
+};
